655a4b288e09d6c52f2dbb7ee431624b629fc9a5
zClapr <48451623+zLuminate@users.noreply.github.com>
2023-05-03 11:39:31 +0200

    comparing numpy algorithms to those on wikipedia

--- dft.py
+++ dft.py
@@ -18,6 +18,24 @@ def fourier_transform(values):
 
     return transformed
 
+def inverse_fourier_transform(values):
+
+    # https://en.wikipedia.org/wiki/Fast_Fourier_transform
+    # https://en.wikipedia.org/wiki/Discrete_Fourier_transform
+
+    N = len(values)
+    transformed = []
+
+    for n in range(N):
+        X = 0
+
+        for k in range(N-1):
+            X += values[k] * e**((2j*pi)/N * k * n)
+
+        transformed.append(X/N)
+
+    return transformed
+
 if __name__ == "__main__":
 
     import numpy as np
@@ -26,17 +44,20 @@ if __name__ == "__main__":
 
     periods = [1,5]
     amplitudes = [1,3]
-    sequence = [0*(random()-0.5) + sum([amplitudes[i]*sin((2*pi)/T*x) for i,T in enumerate(periods)]) for x in np.arange(0, 5, 0.1)]
+    sequence = [2*(random()-0.5) + sum([amplitudes[i]*sin((2*pi)/T*x) for i,T in enumerate(periods)]) for x in np.arange(0, 5, 0.1)]
 
-    data = np.abs(fourier_transform(sequence))
+    data = fourier_transform(sequence)
 
     with open ('sequence.txt', 'w') as f:
         for i in range(len(data)):
             f.write(str(data[i]) + '\n')
 
-    plt.plot(sequence, color='red', label='signal sequence')
-    plt.bar(np.arange(len(data)), data, color='blue', label='dft', width=0.25)
-    # plt.plot(np.abs(np.fft.fft(sequence)), color='green', label='fft (numpy)')
+    plt.plot(sequence, color='red', label='signal sequence', linewidth=5)
+    plt.bar(np.arange(len(data)), np.abs(data), color='blue', label='dft', width=0.25)
+    plt.plot(inverse_fourier_transform(data), color='orange', label='inverse dft')
+
+    plt.bar(np.arange(len(data)), np.abs(np.fft.fft(sequence)), color='green', label='fft (numpy)', width=0.2)
+    plt.plot(np.fft.ifft(np.fft.fft(sequence)), color='purple', label='inverse fft (numpy)')
 
     plt.legend()
     plt.grid()
--- engine.py
+++ engine.py
@@ -4,14 +4,16 @@ import numpy as np
 import cv2
 from time import time
 
+import dft
+
 image = cv2.imread('resources/test.png')
 imageData = np.array(image)
 
-def luminance(pixel, option=1, intify=True):
+def luminance(pixel, option=1, output_as_int=True):
 
     # https://stackoverflow.com/questions/596216/formula-to-determine-perceived-brightness-of-rgb-color
 
-    if intify:
+    if output_as_int:
         if option == 1:
             return int(0.2126*pixel[0] + 0.7152*pixel[1] + 0.0722*pixel[2])
         elif option == 2:
@@ -28,17 +30,10 @@ def luminance(pixel, option=1, intify=True):
 
 start = time()
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.autoscale(enable=True, axis='both', tight=True)
-ax.set_facecolor('lightblue')
-
-for x in range(imageData.shape[0]):
-    for y in range(imageData.shape[1]):
-        grayness = luminance(imageData[x][y], option=1, intify=True)
-        ax.bar3d(x, y, 0, 1, 1, 255-grayness, shade=False, color=str(grayness/255))
+for row in imageData:
+    data = dft.fourier_transform([luminance(pixel, option=1, output_as_int=False) for pixel in row])
+    plt.plot(data)
 
 print("process completed in: " + str(time() - start) + "seconds")
 
-while True:
-    plt.show()
+plt.show()
